0c21a14178b89e2b547fbd96f90034fefebb152d
Ruben Wouters <rubenwouters404@hotmail.com>
2016-06-12 22:53:56 +0200

    Revert "Adding translation for Brazilian Portuguese language (pt-BR)"

--- lang/pt-BR/dashboard.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-
-return [
-    'env_file_empty' => "Parece que uma das credenciais não está configurada corretamente no arquivo .ENV",
-    'not_there_yet' => "Uh oh, ainda falta algum detalhe!",
-    'facebook_login' => "Por favor, faça login com a sua conta do Facebook para vincular a sua página do Facebook.",
-];
